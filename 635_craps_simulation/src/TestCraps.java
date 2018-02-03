a1d34bd9688087909415ec356ef93e6bbb8120cd
seis-602-fall2017 <darius.nyaundi@10.50.70.10>
2018-02-03 12:11:59 -0600

    change to text up top

--- 635_craps_simulation/src/TestCraps.java
+++ 635_craps_simulation/src/TestCraps.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 
+//import test crap
 /**
  * Starting application for running multiple simulations of CrapsGame. This
  * gathers statistics (how many games were a win, and how many were a loss), and
@@ -25,8 +26,8 @@ public class TestCraps
 
 	/**
 	 * CrapsGame represents a single game of craps, which contains a single Dice
-	 * object that is rolled when game.play() is called. This returns true if
-	 * the game is a win, false otherwise.
+	 * object that is rolled when game.play() is called. This returns true if the
+	 * game is a win, false otherwise.
 	 */
 	private CrapsGame game;
 
@@ -37,23 +38,23 @@ public class TestCraps
 	private int numWins;
 
 	/**
-	 * Field <code>numPlays</code> counts the total number of individual games
-	 * to play.
+	 * Field <code>numPlays</code> counts the total number of individual games to
+	 * play.
 	 */
 	private int numPlays;
 
 	/**
-	 * int array field <code>winSteps</code> tracks number of wins w after
-	 * exactly k rolls: <code>winsteps[k]==w </code>, out of
-	 * <code>numPlays</code> total plays.
+	 * int array field <code>winSteps</code> tracks number of wins w after exactly k
+	 * rolls: <code>winsteps[k]==w </code>, out of <code>numPlays</code> total
+	 * plays.
 	 */
 	private int[] winForNumberOfStepsOf = new int[100];
 	private ArrayList<Integer> test;
 
 	/**
-	 * int array field <code>lossSteps</code> racks number of losses l after
-	 * exactly k rolls: <code>lossSteps[k]==l</code>, out of
-	 * <code>numPlays</code> total plays.
+	 * int array field <code>lossSteps</code> racks number of losses l after exactly
+	 * k rolls: <code>lossSteps[k]==l</code>, out of <code>numPlays</code> total
+	 * plays.
 	 */
 	private int[] lossSteps = new int[100];
 
@@ -68,14 +69,14 @@ public class TestCraps
 
 		// we'll use the above to substitute Die subclass objects,
 		// thus affecting the outcome
-	
+
 	}
 
 	/**
-	 * Instance method <code>public void play(int)</code> plays n games of
-	 * craps, tracking the results of each: number of total wins out of n, and
-	 * for each win, how many games end in a win/loss after exactly k plays:
-	 * winSteps[k], lossSteps[k]
+	 * Instance method <code>public void play(int)</code> plays n games of craps,
+	 * tracking the results of each: number of total wins out of n, and for each
+	 * win, how many games end in a win/loss after exactly k plays: winSteps[k],
+	 * lossSteps[k]
 	 * 
 	 * @param n
 	 *            total number of games to play
@@ -95,9 +96,9 @@ public class TestCraps
 	}
 
 	/**
-	 * Instance method <code>public void reportStats</code> prints out final
-	 * value of number of how many games end in a win/loss after exactly k
-	 * plays: winSteps[k], lossSteps[k]total wins out of n, and
+	 * Instance method <code>public void reportStats</code> prints out final value
+	 * of number of how many games end in a win/loss after exactly k plays:
+	 * winSteps[k], lossSteps[k]total wins out of n, and
 	 * 
 	 * It then prints out the winning percentage for all games played.
 	 * 
@@ -108,12 +109,12 @@ public class TestCraps
 		StdOut.println("\nNumber of (wins,losses) for games of given length follow:\n");
 		for (int numSteps = 1; numSteps < winForNumberOfStepsOf.length; numSteps++)
 		{
-			StdOut.println("(wins,losses) ending at " + numSteps + " roll" + ((numSteps > 1) ? "s" : "") + ": (" + winForNumberOfStepsOf[numSteps] + ","
-					+ lossSteps[numSteps] + ")");
+			StdOut.println("(wins,losses) ending at " + numSteps + " roll" + ((numSteps > 1) ? "s" : "") + ": ("
+					+ winForNumberOfStepsOf[numSteps] + "," + lossSteps[numSteps] + ")");
 		}
-		
+
 		StdOut.println("\nPlayed " + numPlays + " games total.");
-		
+
 		StdOut.println("Won " + numWins + "/" + numPlays + "==" + 100.0 * (0.0 + numWins) / numPlays + "%");
 
 	}
@@ -121,8 +122,8 @@ public class TestCraps
 	/**
 	 * TestCraps.main():
 	 * 
-	 * Build a TestCraps object, play NUM_TO_PLAY games, gathering and reporting
-	 * the results.
+	 * Build a TestCraps object, play NUM_TO_PLAY games, gathering and reporting the
+	 * results.
 	 * 
 	 * We turn off console output by CrapsGame.showOutput = false, so that the
 	 * simulation runs more quickly, then turn it back on to report the results
